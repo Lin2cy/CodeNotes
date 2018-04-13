dd52ff0ffa1c16641b26b21927b30f6a60548e6f
lin2cy <jcyamavip@gmail.com>
2018-04-13 16:08:41 +0800

    132

---
+++ golefile/src/main/java/com/file/golefile/GolefileApplication.java
@@ -0,0 +1,12 @@
+package com.file.golefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolefileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolefileApplication.class, args);
+	}
+}
